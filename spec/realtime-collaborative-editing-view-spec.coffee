45c387f312fb1ad6081268ef9d8bad7b41abc462
Eric Adamski <er.adamski@gmail.com>
2016-10-18 17:18:11 -0400

    Updated coffescript to babel

--- spec/realtime-collaborative-editing-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-RealtimeCollaborativeEditingView = require '../lib/realtime-collaborative-editing-view'
-
-describe "RealtimeCollaborativeEditingView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/realtime-collaborative-editing-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RealtimeCollaborativeEditingView from '../lib/realtime-collaborative-editing-view';
+
+describe('RealtimeCollaborativeEditingView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
